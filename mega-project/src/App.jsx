d37ab0357c6518762371a5fa3eccf4e09380992f
Ankit Kumar <ankit1063.be22@chitkarauniversity.edu.in>
2024-07-23 19:49:48 +0530

    lecure 22

--- mega-project/src/App.jsx
+++ mega-project/src/App.jsx
@@ -1,19 +1,39 @@
-
-import { useState } from 'react'
-import './App.css'
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import {Header,Footer} from './components';
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const [loading, setLoadin] = useState(true);
+  const dispatch = useDispatch();
 
-const[loading,setLoadin]=useState(true);
+  useEffect(() => {
+    authService
+      .getCurrentuser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoadin(false));
+  }, []);
 
-  return (
-    <>
-    <h1>A Blog application</h1>
-    <div>
-      Hello ankit
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap  content-between bg-grey-200">
+      <div className="w-full block">
+        <Header />
+        <main>
+        {/* Todo:  <Outlet/> */}
+        </main>
+        <Footer />
+      </div>
     </div>
-    </>
-  )
+  ) : null;
 }
 
-export default App
+export default App;
